b217ba635eeeacb14bc02725abbd46e0c1271fc7
Ajitkumar-1001 <sachinajitkumarp@gmail.com>
2025-08-30 02:19:11 -0400

    Updated last

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer : React.FC = () => {
 
     return( 
         <footer className=" w-full flex flex-col items-center">
-              <div className="fixed-bottom  mt-10 pt-6 border-t max-w-10xl border-gray-700 text-center text-gray-500">
+              <div className="fixed-bottom  mt-10 py-3 border-t max-w-10xl border-gray-700 text-center text-gray-500">
             <h1 className="text-md">Ajitkumar Senthil Kumar</h1>
             <p className="text-sm"> {new Date().getFullYear()} copyrights All Rights Reserved.</p>
             <p className="text-xs mt-1">Licensed under the MIT License.</p>
