90906f390dbd8d18bc64cfb1440cd0dd9eace6ab
Ajitkumar-1001 <sachinajitkumarpr@gmail.com>
2025-09-12 22:33:06 -0400

    Updated the IFrame

--- src/comp/ResponsiveIframe.tsx
+++ src/comp/ResponsiveIframe.tsx
@@ -8,7 +8,7 @@ export interface Props {
 
 
 
-export default function ResponsiveIframe({src,ratio=21/3,className}:Props) {
+export default function ResponsiveIframe({src,ratio=21/14,className}:Props) {
 
 
     const style = { aspectRatio: `${ratio}` }; 
