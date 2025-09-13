d19b7a7566a54f5304713961b40dae8d70dfda97
Ajitkumar-1001 <sachinajitkumarpr@gmail.com>
2025-09-12 22:26:23 -0400

    Responsive Iframe

--- src/comp/Dialog.tsx
+++ src/comp/Dialog.tsx
@@ -6,6 +6,9 @@ import {
  
   DialogTrigger,
 } from "../components/ui/dialog";
+import ResponsiveIframe from "./ResponsiveIframe";
+
+
 
 export interface Demolinks { 
     source : string,
@@ -15,7 +18,7 @@ export interface Demolinks {
 
 }
 
-export default function DialogDemo({source,className,title="Project Preview",button}:Demolinks) {
+export default function DialogDemo({source,className,button}:Demolinks) {
   return (
     
       <Dialog>
@@ -23,9 +26,9 @@ export default function DialogDemo({source,className,title="Project Preview",but
         <DialogTrigger asChild>
           <Button className={`w-25 h-8 bg-gradient-to-tr from-indigo-500 to-blue-800 ${className ?? ' '}`} variant="outline">{button}</Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-10xl md:max-w-8xl backdrop-blur bg-transparent">
+        <DialogContent className="sm:max-w-5xl md:max-w-8xl backdrop-blur bg-transparent">
             <div className="flex flex-col items-center p-2 mx-auto max-w-10xl w-full">
-                <iframe className="flex flex-col items-center" width={1600} height={700} src={source} title={title} ></iframe>
+                <ResponsiveIframe src={source} />
             </div>
         
         
---
+++ src/comp/ResponsiveIframe.tsx
@@ -0,0 +1,32 @@
+
+
+export interface Props { 
+    ratio?: number;
+    src:string; 
+    className?:string;
+};
+
+
+
+export default function ResponsiveIframe({src,ratio=16/9,className}:Props) {
+
+
+    const style = { aspectRatio: `${ratio}` }; 
+
+  return (
+    <div
+      className={`relative w-full overflow-hidden rounded-xl ${className}`}
+      style={style}
+    >
+      <iframe
+        className="absolute inset-0 h-full w-full border-0"
+        src={src}
+     
+        loading="lazy"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        // allowFullScreen
+        referrerPolicy="strict-origin-when-cross-origin"
+      />
+    </div>
+  );
+}
