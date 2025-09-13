f68ae42d7b959efef83985e928876f505bf03e20
Ajitkumar-1001 <sachinajitkumarpr@gmail.com>
2025-09-12 20:52:37 -0400

    Fixed render issues

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -14,7 +14,7 @@ const projects: any = [
     link: "https://github.com/Ajitkumar-1001/citibike_mlops",
     demo: "Live coming soon....",
     source: "https://blogspot-ajit.vercel.app/blogs/featured/blog-2",
-    demo2: "../../public/clip.mp4",
+    demo2: "../.././clip.mp4",
   },
   {
     title: "MIDAS Skin Cancer Detection",
@@ -24,7 +24,7 @@ const projects: any = [
     link: "https://github.com/Ajitkumar-1001/MIDAS-Skin-Cancer-Detection",
     demo: "Live coming soon...",
     source: "https://blogspot-ajit.vercel.app/blogs/featured/blog-1",
-    demo2: "../../public/clip.mp4",
+    demo2: "../.././clip.mp4",
   },
   {
     title: "CovDet",
@@ -34,7 +34,7 @@ const projects: any = [
     link: "https://github.com/Ajitkumar-1001/MIDAS-Skin-Cancer-Detection",
     demo: "Live coming soon....",
     source: "https://blogspot-ajit.vercel.app/blogs/featured/blog-3",
-    demo2: "../../public/clip.mp4",
+    demo2: "../.././clip.mp4",
   },
   {
     title: "BlogPosts",
@@ -44,7 +44,7 @@ const projects: any = [
     link: "https://github.com/Ajitkumar-1001/My_Galaxy",
     demo: "https://blogspot-ajit.vercel.app",
     source: "https://blogspot-ajit.vercel.app/blogs",
-    demo2: "../../public/clip.mp4",
+    demo2: "../.././clip.mp4",
   }, {
     title: "Jersey City Citibike rides Prediction",
     description:
@@ -53,7 +53,7 @@ const projects: any = [
     link: "https://github.com/Ajitkumar-1001/citibike_mlops",
     demo: "Live coming soon....",
     source: "https://blogspot-ajit.vercel.app/blogs/featured/blog-5",
-    demo2:"../../public/clip.mp4",
+    demo2:"../.././clip.mp4",
   },
   {
     title: "New York City Taxi-rides Prediciton",
@@ -63,7 +63,7 @@ const projects: any = [
     link: "https://github.com/Ajitkumar-1001/AppliedML_NYC_taxidata",
     demo: "Live coming soon....",
     source: "https://blogspot-ajit.vercel.app/blogs/featured/blog-6",
-    demo2: "../../public/clip.mp4",
+    demo2: "../.././clip.mp4",
   }
 
 ];
