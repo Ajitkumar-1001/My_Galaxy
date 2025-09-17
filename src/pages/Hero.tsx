b0bfe3e8062912f2151e8c3265d9937ef2715ed4
Ajitkumar-1001 <sachinajitkumarpr@gmail.com>
2025-09-17 01:58:03 -0400

    Added Resume

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -149,7 +149,7 @@ const Hero: React.FC = () => {
 
           <motion.a
             variants={controlprops2 as any}
-            href="/Ajitkumar_Resume.pdf"
+            href="../.././Ajitkumar_Resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Open resume in a new tab"
