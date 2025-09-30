da954e82bfa54134e72f3a02ba196a404508e8a9
Ajitkumar-1001 <sachinajitkumarpr@gmail.com>
2025-09-30 10:49:43 -0400

    responsive iframe fix

--- src/comp/ResponsiveIframe.tsx
+++ src/comp/ResponsiveIframe.tsx
@@ -15,13 +15,13 @@ export default function ResponsiveIframe({src,ratio=21/16,className}:Props) {
 
   return (
     <div
-      className={`relative w-full overflow-hidden max-w-6xl rounded-xl ${className}`}
+      className={`relative w-full h-full overflow-hidden max-w-6xl rounded-xl ${className}`}
       style={style}
     >
       <iframe
-        className="absolute inset-0 h-full w-full border-0"
+        className="absolute inset-0 w-full h-full md:h-full h-[75%] border-0"
         src={src}
-     
+
         loading="lazy"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         // allowFullScreen
